bfae4a02f7bb92378479d3b98ba23b1063273924
飛_鱼 <1058209187@qq.com>
2018-08-29 18:08:57 +0800

    "

--- multithread/thread_demo04/src/com/shenrs/Test004.java
+++ multithread/thread_demo04/src/com/shenrs/Test004.java
@@ -1,6 +1,8 @@
 package com.shenrs;
 
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author shenrs
@@ -8,8 +10,32 @@ import java.util.concurrent.ConcurrentLinkedDeque;
  * @create 2018-08-28 17:36
  **/
 public class Test004 {
-    public static void main(String[] args) {
-        // 无界队列
-        ConcurrentLinkedDeque concurrentLinkedDeque = new ConcurrentLinkedDeque();
+    public static void main(String[] args) throws InterruptedException {
+        // 无界
+        ConcurrentLinkedDeque q = new ConcurrentLinkedDeque();
+        q.offer("余胜军");
+        q.offer("码云");
+        q.offer("蚂蚁课堂");
+        q.offer("张杰");
+        q.offer("艾姐");
+        //从头获取元素,删除该元素
+        System.out.println(q.poll());
+        //从头获取元素,不刪除该元素
+        System.out.println(q.peek());
+        //获取总长度
+        System.out.println(q.size());
+
+        // 阻塞队列
+        ArrayBlockingQueue arrayBlockingQueue = new ArrayBlockingQueue(3);
+        arrayBlockingQueue.add("张三");
+        arrayBlockingQueue.add("李四");
+        arrayBlockingQueue.add("王武");
+        //可以阻塞的队列
+        boolean offer = arrayBlockingQueue.offer("王麻衣子", 2, TimeUnit.SECONDS);
+        System.out.println(arrayBlockingQueue.size()+"---"+offer);
+        System.out.println(arrayBlockingQueue.poll());
+        System.out.println(arrayBlockingQueue.poll());
+        System.out.println(arrayBlockingQueue.poll());
+        System.out.println(arrayBlockingQueue.poll());
     }
 }
